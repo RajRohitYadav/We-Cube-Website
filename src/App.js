6d66a4c227308123673ed31b2811deeacafbe676
raj <rajrohit1997@gmail.com>
2019-07-27 10:33:53 +0530

    Responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import webcube_logo from './wecube_logo.jpg';
-import Popup from "reactjs-popup";
 
 class App extends React.Component {
   constructor(props){
@@ -21,12 +20,12 @@ class App extends React.Component {
       {/* Navbar */}
       <div id='header'>
         <img src={webcube_logo} height="60" width="150" style={{position: 'fixed',left:'10px',top:'10px'}}/>
-        {/* <img src={webcube_logo} height="60" width="150" style={{position: 'fixed',right:'10px',padding:'10px'}}/> */}
         <div onClick={()=>{this.showMenu(true)}} style={{position: 'fixed',right:'10px',backgroundColor: 'black',cursor:'pointer',width:'50px',height:'50px'}}>
           <div className="menu icon" style={{position: 'fixed',right:'10px',top:'28px',color: 'white',cursor:'pointer'}}></div>
         </div>
       </div>
 
+      {/* POPUP */}
       {this.state.showMenu&&
         <div className='popup'>
           <div className='popup_inner'>
@@ -45,6 +44,13 @@ class App extends React.Component {
             </div>
 
             <div className='popup_section_button'>
+              <a href="#services" style={{color:'white',textDecoration:'none'}}
+              onClick={()=>{this.showMenu(false)}}>
+                Our Service
+              </a>
+            </div>
+
+            <div className='popup_section_button'>
               <a href="#contact" style={{color:'white',textDecoration:'none'}}
               onClick={()=>{this.showMenu(false)}}>
                 Contact
@@ -55,23 +61,107 @@ class App extends React.Component {
       }
 
       {/* Page 1 */}
-      <div id='about' style={{height:'100vh',backgroundColor:'black',display:'block'}}>
-        <div style={{position:'absolute',marginTop:'calc(50px + 20vmin)',width:'90%',padding:'5px'}}>
-            <div style={{borderTop: '1px solid white',width:'15%', display: 'inline-block',height:'15px'}}></div>
-            <div style={{display: 'inline-block',fontSize: 'calc(28px + 2vmin)',fontWeight: '800',color:'white',marginLeft:'30px'}}>About Us</div>
-            <div style={{color:'white',width:'60%',marginLeft:'calc(15% + 30px)',marginTop:'10px',fontSize:'calc(12px + 1vmin)'}}>
+      <div id='about' className='page'>
+        <div className='page-content-box'>
+            
+            <div className='big-line'></div>
+            <div className='title'>About Us</div>
+            <div className='data'>
               <br/>
+              <p>
               We Cube is created for an Amazing service; we believe that multitalented people put together to work together can give the best services to our clients. Our work speaks louder than the words. We assure to make your vision a reality.
+              </p>
+            </div>
+
+            <br/><br/><br/>
+
+            <div className='small-line'></div>
+            <div className='title'>Vision</div>
+            <div className='data'>
+              <br/>
+              <p>
+              Our main aim is to build a tremendous network which holds almost every brand and to become a massive source for the corporate world.
+              </p>
+            </div>
+            
+            <br/><br/><br/>
+
+            <div className='d-none d-sm-block'>
+              <div className='small-line'></div>
+              <div className='title'>Mission</div>
+              <div className='data'>
+                <br/>
+                <p>
+                To help the start-up companies, To maintain a good position in the market, To be a well known and reputable company.
+                </p>
+              </div>
             </div>
         </div>
       </div>
-      
+
       {/* Page 2 */}
-      <div id='contact' style={{height:'100vh',backgroundColor:'black',display:'block'}}>
-        <div style={{position:'absolute',marginTop:'calc(50px + 20vmin)',width:'90%',padding:'5px'}}>
-            <div style={{borderTop: '1px solid white',width:'15%', display: 'inline-block',height:'10px'}}></div>
-            <div style={{display: 'inline-block',fontSize: 'calc(28px + 2vmin)',fontWeight: '900',color:'white',marginLeft:'30px'}}>Contact</div>
-            <div style={{color:'white',width:'60%',marginLeft:'calc(15% + 30px)',marginTop:'10px',fontSize:'calc(12px + 1vmin)'}}>
+      <div id='services' className='page'>
+        <div className='page-content-box'>
+          <div className='big-line'></div>
+          <div className='title'>Our Service</div>
+
+          {/* Page 2 Section 1 */}
+          <div className='services-data row'>
+            <div className='services-data-title col-md-3'>
+              <h6>CORPORATE STATIONARIES PRIMARY</h6>
+              <p>Business card, Letterhead, Envelopes, Mailing Label, Invoices, Word template letterhead, Email signature template</p>
+            </div>
+            
+            <div className='services-data-title col-md-3'>
+              <h6>CORPORATE STATIONARIES SECONDARY</h6>
+              <p>Continuation sheet, Packing Tapes, Badges for visitors, Employee ID Card, Fax sheet, PPT, Diary Cover, Post its, Planner, T-shirts, Memo pads
+              </p>
+            </div>
+
+            <div className='col-md-3'>
+              <h6>UI/UX & DEVELOPMENT</h6>
+              <p>Web design, e-commerce website, App Design, E-Mailer, Development (Wordpress, Html ,css, java script, PHP, jQuery, MySQL )
+              </p>
+            </div>
+
+            <div className='col-md-3 d-none d-sm-block'>
+              <h6>COLLATERALS</h6>
+              <p>brochure, poster, Flyers, Hoardings</p>
+            </div>
+          </div>
+
+          {/* Page 2 Section 2 */}
+          <div className='services-data row d-none d-md-flex' style={{marginTop:'20vim'}}>
+            <div className='col-md-3'>
+              <h6>DIGITAL MARKETING</h6>
+              <p>SEO & SMM, Email / SMS</p>
+            </div>
+
+            <div className='col-md-3'>
+              <h6>MARKETING COLLATERAL</h6>
+              <p>Facebook, Instagram, Linked In, Twitter, Google, eCommerce listing, Newsletter</p>
+            </div>
+
+            <div className='col-md-3'>
+              <h6>MAINTENANCE</h6>
+              <p>Website Creating & Hosting, Web Security, Data Storage & Management, Data Backup & Malware Removal, Ssl Crft</p>
+            </div>
+
+            <div className='col-md-3'>
+              <h6>MOTION</h6>
+              <p>Infographics, Motion Graphics, GIF’s</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      
+      {/* Page 3 */}
+      <div id='contact' className='page'>
+        <div className='page-content-box'>
+            <div className='big-line'></div>
+            <div className='title'>Contact</div>
+            <div className='data'>
               <br/>
                 Address<br/>
                 <div style={{color:'gray',marginTop:'10px'}}>
@@ -84,35 +174,25 @@ class App extends React.Component {
                   wecube.info@gmail.com<br/>
                   (+91)7406610475 | (+91)9591552012
                 </div>
+                
+            </div>
+            <div className='contact-map d-none d-md-flex'>
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9421.067291147843!2d77.62151498995459!3d12.938059102390216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1447b6576acb%3A0x5514c1bedc3c55df!2sNR+Residency!5e0!3m2!1sen!2sin!4v1564167038568!5m2!1sen!2sin" 
+                width="400" 
+                height="300" 
+                frameborder="0"
+              />
             </div>
         </div>
       </div>
-      {/* <div style={{height:'100vh',backgroundColor:'green',display:'block'}}>
-      <div style={{width: '15%',borderTop: '1px solid white',position: 'absolute',left: 10,top:'125%'}}></div>
-        <h1 style={{ marginLeft: '18%',position: 'absolute',color: 'white',width:'60%',fontSize: 'calc(20px + 2vmin)',fontWeight: '800',top:'118%'}}> */}
-      {/* <div style={{height:'100vh',backgroundColor:'#363435',display:'block'}}>
-        <span className="line"></span>
-        <p className='heading'> Contact Us</p>
-        <div style={{marginLeft: '18%',position: 'absolute',color: 'white',width:'50%',top:'130%'}}>
-                Address<br/>
-                #74/2A, Flat No.109, NR Residency, K.R.<br/> Garden, 1st Cross, 8th Block, Koramangala,<br/> Bangalore-560095
-                <br/><br/>
-                Mail/Tel<br/>
-                We cube.info@gmail.com<br/>
-                (+91)7406610475 | (+91)9591552012
-        </div>
-      </div> */}
 
-      {/* Page 3 */}
-      {/* <div style={{height:'100vh',backgroundColor:'black'}}>
-
-      </div> */}
-
+      {/* FOOTER */}
       <div id="footer">
-          <a href="#" className="fa fa-facebook"></a>
-          <a href="#" className="fa fa-twitter"></a>
-          <a href="#" className="fa fa-dribbble"></a>
-          <a href="#" className="fa fa-instagram"></a>
+          <a target="_blank" href="https://www.facebook.com/wecube.net.37" className="fa fa-facebook"></a>
+          <a target="_blank" href="https://www.linkedin.com/company/we-cube-network" className="fa fa-linkedin"></a>
+          <a  href="" className="fa fa-pinterest"></a>
+          <a target="_blank" href="https://www.instagram.com/wecubenetwork/" className="fa fa-instagram"></a>
       </div>
     </div>
   );}
